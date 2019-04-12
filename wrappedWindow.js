3f7112eccaabb8f5f288d391fa4cb3f022f1c990
Pascal Ronecker <pascal.ronecker@boxtal.com>
2019-04-12 08:39:16 +0200

    minor tweak again to follow link in gchat 'cards' link

--- wrappedWindow.js
+++ wrappedWindow.js
@@ -122,7 +122,7 @@ module.exports = function createWrappedWindow(opts) {
   });
 
   window.webContents.on('dom-ready', () => {
-    window.webContents.executeJavaScript('var ipc = require(\'electron\').ipcRenderer; document.addEventListener("click", (evt) => { if (evt.target && evt.target.localName != "a" && evt.target.localName != "input"){evt.preventDefault();} else if (evt.target && evt.target.target == "_blank" && evt.target.href.startsWith("http")) { ipc.send("open-link", evt.target.href); evt.preventDefault(); } }, true);', true);
+    window.webContents.executeJavaScript('var ipc = require(\'electron\').ipcRenderer; document.addEventListener("click", (evt) => { if (evt.target && evt.target.parentNode && evt.target.parentNode.href ){console.log("trouloulou"); ipc.send("open-link", evt.target.parentNode.href); evt.preventDefault(); }else if (evt.target && evt.target.localName != "a" && evt.target.localName != "input"){evt.preventDefault();} else if (evt.target && evt.target.target == "_blank" && evt.target.href.startsWith("http")) { ipc.send("open-link", evt.target.href); evt.preventDefault();}}, true);', true);
     window.webContents.executeJavaScript('var ipc = require(\'electron\').ipcRenderer; var fi = document.querySelector("link#favicon256"); ipc.send("favicon-changed", fi.href); var callback = function(mutationList) { ipc.send("favicon-changed", fi.href); }; var observer = new MutationObserver(callback); observer.observe(fi, { attributes: true });');
   });
 
