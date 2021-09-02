c7e10bba54d1b52c2d3a5d29ea6622cd2403d9a2
squalou <squalou.jenkins@gmail.com>
2021-09-02 20:02:37 +0200

    fix #38 electron 14 - native filechooser support - add dependencies in packages

--- google-chat-linux.sh
+++ google-chat-linux.sh
@@ -3,6 +3,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $SCRIPTPATH
 export NODE_PATH="$SCRIPTPATH/node_modules/.bin"
 export PATH=$NODE_PATH:$PATH
-
+# GTK_USE_PORTAL=1 is set from index.js
 ${NODE_PATH}/electron  "${SCRIPTPATH}/src/index.js" --trace-warnings
 
