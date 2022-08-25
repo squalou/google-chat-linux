e24fba6d365d90ef2520d7fe149e478e1ee245c9
Andrew Sidhu <sid@foyer.ai>
2022-08-25 14:37:45 -0400

    FIX: Systray left click & display on Wayland. Main window handling
    FIX: Systray left click & display on Wayland. Main window handling
    
    Three broken behaviors were addressed in this commit.
    
    1. Fixes systray not appearing on wayland systems due to incorrect resource paths when building a stand alone application. Icon resources have been added to the electron builder configuration to be packaged with the app and paths.js updated accordingly.
    
    BREAKING CHANGE: 
    Before this fix, systray worked with `npm start` or `electron .` when developing. Now it will only work once the application has been packed. Can be refactored to detect its environment and determine the correct path.
    
    2. Fixes left click functionality for the systray by updating the electron wrapper and removing the libappindicator-* stub hack. Systray clicking and context menu now work as intended.
    
    3. Fixes main window management to be responsive to GTK3 widget manipulation such as i3/sway kill window by updating electron wrapper.
    
    Additional Chores:
    
    1. Removed `fs` stub package and `path` from npm dependencies, both are core modules of Node.js and present an opportunity for package hijacking.
    
    2. Removed package `electron-localshortcut`, unused package.
    
    3. Bump `electron-context-menu` version. Fixes compatibity with latest electron wrapper.
    
    4. Specified latest LTS node engine in package.json for better visibility into development.
    
    5. Version bumped to 5.20.18-1

--- src/paths.js
+++ src/paths.js
@@ -2,9 +2,9 @@ const path = require("path");
 const {app} = require("electron");
 
 module.exports = {
-	"configsPath" : path.join(app.getPath("appData"), "google-hangouts-chat-linux.json"),
-	"iconPath" : path.join(__dirname, "../assets/icon/icon.png"),
-	"NORMAL" : path.join(__dirname, "../assets/icon/normal-64.png"),
-	"BADGE" : path.join(__dirname, "../assets/icon/badge-64.png"),
-	"OFFLINE" : path.join(__dirname, "../assets/icon/offline-64.png")
+	"configsPath" : path.resolve(app.getPath("appData"), "google-hangouts-chat-linux.json"),
+	"iconPath" : path.resolve(process.resourcesPath, "icon/icon.png"),
+	"NORMAL" : path.resolve(process.resourcesPath, "icon/normal-64.png"),
+	"BADGE" : path.resolve(process.resourcesPath, "icon/badge-64.png"),
+	"OFFLINE" : path.resolve(process.resourcesPath, "icon/offline-64.png")
 }
