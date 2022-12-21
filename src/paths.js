5ad6153ccea470ae0c56166fd45f5d9709fdcd09
squalou <squalou.github@ronecker.fr>
2022-12-21 13:20:22 +0100

    Revert "Fix system tray and app icon paths (#60)"
    This reverts commit 3a8c6b5c2a466012a7deb66651c70e6d84c01ed8.

--- src/paths.js
+++ src/paths.js
@@ -3,8 +3,8 @@ const {app} = require("electron");
 
 module.exports = {
 	"configsPath" : path.resolve(app.getPath("appData"), "google-hangouts-chat-linux.json"),
-	"iconPath" : path.resolve(path.dirname(__dirname), "assets", "icon", "icon.png"),
-	"NORMAL" : path.resolve(path.dirname(__dirname), "assets", "icon", "normal-64.png"),
-	"BADGE" : path.resolve(path.dirname(__dirname), "assets", "icon", "badge-64.png"),
-	"OFFLINE" : path.resolve(path.dirname(__dirname), "assets", "icon", "offline-64.png")
+	"iconPath" : path.resolve(process.resourcesPath, "icon/icon.png"),
+	"NORMAL" : path.resolve(process.resourcesPath, "icon/normal-64.png"),
+	"BADGE" : path.resolve(process.resourcesPath, "icon/badge-64.png"),
+	"OFFLINE" : path.resolve(process.resourcesPath, "icon/offline-64.png")
 }
