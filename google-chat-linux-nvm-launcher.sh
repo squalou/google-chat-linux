d711db5e2183e49cfc2d99510253e9b78e381565
squalou <squalou.jenkins@gmail.com>
2023-05-24 21:51:41 +0200

    google-chat-linux.sh can be called with electron command line parameters directly

--- google-chat-linux-nvm-launcher.sh
+++ google-chat-linux-nvm-launcher.sh
@@ -3,5 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use --lts
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$SCRIPT_DIR/google-chat-linux.sh
+$SCRIPT_DIR/google-chat-linux.sh # --enable-features=UseOzonePlatform --ozone-platform=wayland
 
--- google-chat-linux.sh
+++ google-chat-linux.sh
@@ -4,5 +4,6 @@ echo $SCRIPTPATH
 export NODE_PATH="$SCRIPTPATH/node_modules/.bin"
 export PATH=$NODE_PATH:$PATH
 # GTK_USE_PORTAL=1 is set from index.js
-${NODE_PATH}/electron  "${SCRIPTPATH}/src/index.js" --trace-warnings
+# for wayland : --enable-features=UseOzonePlatform --ozone-platform=wayland
+${NODE_PATH}/electron  "${SCRIPTPATH}/src/index.js" --trace-warnings "$@" 
 
