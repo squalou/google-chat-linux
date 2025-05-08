a13ef6fec3f680e40d740cb3068a4283f6f2ec56
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-05-08 21:08:07 +0200

    set zoom-in and zoom-out shortcuts

--- src/keyboard.js
+++ src/keyboard.js
@@ -13,6 +13,16 @@ const goForward = () => {
     }
 }
 
+const zoomIn = () => {
+  const currentZoom = mainWindow.webContents.getZoomFactor();
+  mainWindow.webContents.setZoomFactor(currentZoom + 0.1);
+};
+
+const zoomOut = () => {
+  const currentZoom = mainWindow.webContents.getZoomFactor();
+  mainWindow.webContents.setZoomFactor(currentZoom - 0.1);
+};
+
 const registerKeyboardShortcuts = (windowObj) => {
     mainWindow = windowObj;
 
@@ -22,6 +32,27 @@ const registerKeyboardShortcuts = (windowObj) => {
 
     globalShortcut.register("Alt+Left", () => {
         goBack();
+    });
+
+    // Handle different keyboard layouts for zoom-in and zoom-out
+    // for example: on some layouts,
+    // the "+" key may be represented as "Shift+=" and in some other just as "+"
+    mainWindow.webContents.on("before-input-event", (event, input) => {
+        const isMac = process.platform === "darwin";
+        const commandOrControl = isMac ? input.meta : input.control;
+        const isZoomInShortcut = (commandOrControl && input.key === "+");
+        const isZoomOutShortcut = (commandOrControl && input.key === "-");
+
+        if (isZoomInShortcut) {
+            zoomIn();
+            event.preventDefault();
+            return
+        }
+        if (isZoomOutShortcut) {
+            zoomOut();
+            event.preventDefault();
+            return
+        }
     });
 };
 
